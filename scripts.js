bbe8e108007496cd3238de48dcd36b20b4bbdfe4
Vuyisanani Meteni <vuyisanani.meteni@SAMAC133.local>
2018-02-07 18:52:35 +0200

    All files

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ Use straight javascript and avoid JQuery. You can use any resource to set this u
 Some hints are given below but you can use any structure or approach of your choosing.
 
 */
-
+/*
 var button = document.querySelector('.targetClassName');
 
 button.addEventListener('click', function(e) {
@@ -19,4 +19,24 @@ function myFunction() {
         x.style.display = "flex";
     } else {
         x.style.display = "none";
+    }
+    
+    var flexi-container = document.getElementById("flexi-container"),
+    button = document.getElementById("flexi-container");
+    if (flexi-container.style.display === "none"){
+        flexi-container.style.display = "flex";
+        flexi-container.value = "HIDE CONTENT";
+    } else {
+        flexi-container.style.display = "none";
+        button.value = "SHOW CONTENT";
+    }
+    */
+    
+    function myFunction() {
+        var x = document.getElementById("flexi-container");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
     }
